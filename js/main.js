bda7536d5840e6a60edc1973031712d636e8fc03
Gibagood <63713936+Gibagood@users.noreply.github.com>
2020-10-16 11:55:58 +0300

    добавляет main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,18 @@
 (function main() {
   const NAMES = ['Ozzy', 'Billy', 'Jimmy', 'Paul', 'John', 'Till', 'James', 'Flea'];
   const QUANTITY_IMG = 25;
-  const QUANTITY_AVATAR = 6;
   const MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
   const MIN_LIKES = 15;
   const MAX_LIKES = 200;
+  const MIN_COMMENTS = 1;
+  const MAX_COMMENTS = 5;
 
   const picturesList = document.querySelector('.pictures');
   const pictureTemplate = document.querySelector('#picture')
     .content
     .querySelector('.picture');
-  /*
-  const pictures(num) = {
-    url: `photos/${num}.jpg`,
-    likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
-    comments: [
-      {
-        name: getRandomIndex(NAMES),
-        avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
-        message: getRandomIndex(MESSAGE),
-      },
-    ],
-  }; */
 
-  const getrandomIndex = function getrandomIndex(array) {
+  const getRandomIndex = function getrandomIndex(array) {
     const randomNum = Math.floor(Math.random() * array.length);
     const randomNumIndex = array[randomNum];
     return randomNumIndex;
@@ -34,17 +23,42 @@
     return randomNum;
   };
 
-  const renderPicture = function renderPicture() {
+  const getCommentArray = function getCommentArray() {
+    const newComment = [];
+    const QUANTITY_COMMENTS = getRandomNumber(MIN_COMMENTS, MAX_COMMENTS);
+    for (let i = 0; i <= QUANTITY_COMMENTS; i += 1) {
+      i = {
+        name: getRandomIndex(NAMES),
+        avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+        message: getRandomIndex(MESSAGE),
+      };
+      newComment.push(i);
+    }
+    console.log(newComment);
+    return newComment;
+  };
+
+  const getImageArray = function getImageArray(item) {
+    const imageArray = {
+      url: `photos/${item}.jpg`,
+      description: 'описание фотографии',
+      likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+      comments: getCommentArray().length,
+    };
+    return imageArray;
+  };
+
+  const getRenderPicture = function renderPicture(item) {
     const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = `photos/${getRandomNumber(1, QUANTITY_IMG)}.jpg`;
-    pictureElement.querySelector('.picture__comments').textContent = getrandomIndex(MESSAGE);
-    pictureElement.querySelector('.picture__likes').textContent = getRandomNumber(MIN_LIKES, MAX_LIKES);
-    return renderPicture;
+    pictureElement.querySelector('.picture__img').src = getImageArray(item).url;
+    pictureElement.querySelector('.picture__comments').textContent = getImageArray(item).comments;
+    pictureElement.querySelector('.picture__likes').textContent = getImageArray(item).likes;
+    return pictureElement;
   };
 
   const fragment = document.createDocumentFragment();
   for (let i = 1; i <= QUANTITY_IMG; i += 1) {
-    fragment.appendChild(renderPicture());
+    fragment.appendChild(getRenderPicture(i));
   }
   picturesList.appendChild(fragment);
 }());
