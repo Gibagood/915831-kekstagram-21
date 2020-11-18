aa37722f910984277920d00dbe4eb2603e1e1a39
Gibagood <63713936+Gibagood@users.noreply.github.com>
2020-11-18 09:30:22 +0300

    добавляет сообщения о загрузке

--- js/modal.js
+++ js/modal.js
@@ -6,6 +6,15 @@
   const fileInput = document.querySelector(`#upload-file`);
   const buttonUploadCancel = document.querySelector(`#upload-cancel`);
   const imgUploadForm = document.querySelector(`.img-upload__form`);
+  const errorTemplate = document.querySelector(`#error`)
+  .content
+  .querySelector(`.error`);
+
+  const successTemplate = document.querySelector(`#success`)
+  .content
+  .querySelector(`.success`);
+  const successButton = successTemplate.querySelector(`.success__button`);
+  const errorButton = errorTemplate.querySelector(`.error__button`);
 
 
   fileInput.addEventListener(`change`, function () {
@@ -24,6 +33,7 @@
       closeFileInput();
     });
 
+
     imgUploadForm.addEventListener(`submit`, function (evt) {
       window.upload(new FormData(imgUploadForm), function () {
         closeFileInput();
@@ -31,10 +41,40 @@
       evt.preventDefault();
     });
 
+
+    const closeModal = function () {
+      window.modal.imgUploadForm.reset();
+      window.modal.fileInput.value = ``;
+    };
+
+    window.successHandler = function () {
+      const fragment = document.createDocumentFragment();
+      const successElement = successTemplate.cloneNode(true);
+      fragment.appendChild(successElement);
+      document.querySelector(`main`).appendChild(fragment);
+      successButton.addEventListener(`click`, function (evt) {
+        evt.preventDefault();
+        successTemplate.remove();
+        closeModal();
+      });
+    };
+
+
+    window.errorHandler = function () {
+      const fragment = document.createDocumentFragment();
+      const errorElement = errorTemplate.cloneNode(true);
+      fragment.appendChild(errorElement);
+      document.querySelector(`main`).appendChild(fragment);
+      errorButton.addEventListener(`click`, function () {
+        errorTemplate.classList.add(`hidden`);
+        closeModal();
+      });
+    };
   });
 
   window.modal = {
     formUpload,
     imgPreview,
+    fileInput
   };
 })();
--- js/uploadData.js
+++ js/uploadData.js
@@ -2,14 +2,34 @@
 
 (function () {
   const URL = `https://21.javascript.pages.academy/kekstagram`;
+  const StatusCode = {
+    OK: 200
+  };
+  const TIMEOUT_IN_MS = 10000;
 
-  window.upload = function (data, onSuccess) {
+
+  window.upload = function (data, onSuccess, onError) {
     const xhr = new XMLHttpRequest();
     xhr.responseType = `json`;
 
     xhr.addEventListener(`load`, function () {
-      onSuccess(xhr.response);
+      if (xhr.status === StatusCode.OK) {
+        window.successHandler();
+        onSuccess(xhr.response);
+      } else {
+        window.errorHandler();
+        onError(`Статус ответа: ` + xhr.status + ` ` + xhr.statusText);
+      }
     });
+    xhr.addEventListener(`error`, function () {
+      onError(`Произошла ошибка соединения`);
+    });
+    xhr.addEventListener(`timeout`, function () {
+      onError(`Запрос не успел выполниться за ` + xhr.timeout + `мс`);
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
 
     xhr.open(`POST`, URL);
     xhr.send(data);
