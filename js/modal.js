e1c6fb41ae853500dcd7e253756749ed0197c2a2
Gibagood <fotogibadullin@gmail.com>
2021-02-12 16:59:34 +0300

    modal new

--- js/modal.js
+++ js/modal.js
@@ -15,12 +15,12 @@
   .querySelector(`.success`);
 
 
-  fileInput.addEventListener(`change`, function () {
+  fileInput.addEventListener(`change`, function () { // открытие модалки для загрузки картинки
     formUpload.classList.remove(`hidden`);
     document.body.classList.add(`modal-open`);
     window.sizeImage.scaleControlValue.value = `100%`;
 
-    const closeFileInput = function () {
+    const closeFileInput = function () { // закрытие модалки загрузки картинки
       window.modal.imgPreview.style.filter = `none`;
       window.modal.imgPreview.style.transform = `scale(1)`;
       formUpload.classList.add(`hidden`);
@@ -32,60 +32,49 @@
       document.removeEventListener(`keydown`, closeEscUploadCancel);
     };
 
-    const closeButtonUploadCancel = function (evt) {
+    const closeButtonUploadCancel = function (evt) { // кнопка закрытия модалки загрузки картинки
       evt.preventDefault();
       closeFileInput();
     };
-    buttonUploadCancel.addEventListener(`click`, closeButtonUploadCancel);
+    buttonUploadCancel.addEventListener(`click`, closeButtonUploadCancel); // слушатель кнопки закрытия
 
-    const closeEscUploadCancel = function (evt) {
+    const closeEscUploadCancel = function (evt) { // esc для закрытия модалки загрузки картинки
       if (evt.key === `Escape`) {
         evt.preventDefault();
         closeFileInput();
       }
     };
-    document.addEventListener(`keydown`, closeEscUploadCancel);
+    document.addEventListener(`keydown`, closeEscUploadCancel); // слушатель esc
 
-    const closeMessage = function () {
-      window.modal.imgPreview.style.filter = `none`;
-      window.modal.imgPreview.style.transform = `scale(1)`;
-      window.effects.effectField.classList.add(`hidden`);
-      fileInput.value = ``;
-      imgUploadForm.reset();
-      const successMessage = document.querySelector(`.success`);
-      document.querySelector(`main`).removeChild(successMessage);
-      /*  const errorMessage = document.querySelector(`.error`);
-      errorMessage.classList.add(`hidden`); */
-    };
 
-    const clickOnButton = function (evt) {
+    const clickOnButton = function (evt) { // кнопка ок сообщение
       if (evt.target.matches(`.success__button`)) {
         evt.preventDefault();
         closeMessage();
       }
     };
 
-    const clickAround = function (evt) {
+    const clickAround = function (evt) { // закрытие по нажатию всего кроме окна модалки
       if (!evt.target.matches(`.success__inner`)) {
         evt.preventDefault();
         closeMessage();
       }
     };
 
-    const clickEsc = function (evt) {
+    const clickEsc = function (evt) { // esc окна моадлки с сообщением
       if (evt.key === `Escape`) {
         evt.preventDefault();
         closeMessage();
       }
     };
 
-    const successHandler = function () {
+    const successHandler = function () { // слушатели для модалки с сообщением
       document.addEventListener(`click`, clickOnButton);
       document.querySelector(`.success`).addEventListener(`click`, clickAround);
       document.addEventListener(`keydown`, clickEsc);
     };
 
-    window.successModal = function () {
+    window.successModal = function () { // создание модалки сообщения из template
       const fragment = document.createDocumentFragment();
       const successElement = successTemplate.cloneNode(true);
       fragment.appendChild(successElement);
@@ -95,27 +84,27 @@
       successHandler();
     };
 
-    const clickOnAnotherLoad = function (evt) {
+    const clickOnAnotherLoad = function (evt) { // клик по кнопке error
       if (evt.target.matches(`.error__button`)) {
         evt.preventDefault();
         closeMessage();
       }
     };
 
-    const clickArroundError = function (evt) {
+    const clickArroundError = function (evt) { // клик вокруг модалки сообщения error
       if (!evt.target.matches(`.error__inner`)) {
         evt.preventDefault();
         closeMessage();
       }
     };
 
-    const errorHandler = function () {
+    const errorHandler = function () { // слушатели сообщения error
       document.addEventListener(`click`, clickOnAnotherLoad);
       document.addEventListener(`keydown`, clickEsc);
       document.querySelector(`.error`).addEventListener(`click`, clickArroundError);
     };
 
-    window.errorModal = function () {
+    window.errorModal = function () { // создание модалки сообщения из template
       const fragment = document.createDocumentFragment();
       const errorElement = errorTemplate.cloneNode(true);
       fragment.appendChild(errorElement);
@@ -125,7 +114,19 @@
       errorHandler();
     };
 
-    imgUploadForm.addEventListener(`submit`, function (evt) {
+    const closeMessage = function () { // закрытие модалки с сообщением
+      window.modal.imgPreview.style.filter = `none`;
+      window.modal.imgPreview.style.transform = `scale(1)`;
+      window.effects.effectField.classList.add(`hidden`);
+      fileInput.value = ``;
+      imgUploadForm.reset();
+      const successMessage = document.querySelector(`.success`);
+      successMessage.remove();
+      /*  const errorMessage = document.querySelector(`.error`);
+      errorMessage.classList.add(`hidden`); */
+    };
+
+    imgUploadForm.addEventListener(`submit`, function (evt) { // отправка формы
       let formData = new FormData(imgUploadForm);
       window.upload(formData, function () {
         closeFileInput();
